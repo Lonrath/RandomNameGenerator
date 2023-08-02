935ca8780f0af83f97766969bf8f9dd95d26be95
Kaan Aydın <133776792+Lonrath@users.noreply.github.com>
2023-08-02 16:10:22 +0300

    Add files via upload

---
+++ RandomNameGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeView.Model
+{
+    public class RandomNameGenerator
+    {
+        private Random random;
+
+        public RandomNameGenerator()
+        {
+            random = new Random();
+        }
+
+        public string GenerateString(int length)
+        {
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                char randomLetter = (char)random.Next('A', 'Z' + 1);
+                nameBuilder.Append(randomLetter);
+            }
+
+            return nameBuilder.ToString();
+        }
+    }
+}
